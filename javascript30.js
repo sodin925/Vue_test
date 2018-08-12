54cf889c326bb36f51c7756837357b48d26b1b71
sodin925 <30490685+sodin925@users.noreply.github.com>
2018-08-12 13:58:14 +0800

    .

---
+++ javascript30.js
@@ -0,0 +1,6 @@
+/*jshint esversion: 6 */
+let x = document.querySelector('#container');
+x.addEventListener('click', function() {
+  x.innerHTML('456');
+});
+// let newName = JSON.parse(JSON.stringify(name));
